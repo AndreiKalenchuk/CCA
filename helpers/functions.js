4bd4ab5a4e3ad331868416bd4abd78e02a877389
Amdrei Kalianchuk <andrey.kalenchuk@gmail.com>
2020-07-02 23:01:24 -0400

    add create random input numbers create smoke test for default counter

--- helpers/functions.js
+++ helpers/functions.js
@@ -1,6 +1,37 @@
 import {selectorsGen, selectorsCnt} from './../data/selectors.json';
+import {expectedGen, expectedDCF} from './../data/expected.json'
 
-function inputNumber (str, value) {
+function open() {
+    browser.url('https://likejean.github.io/homework-5/');
+    expect(browser).toHaveTitle(expectedGen.title,
+        {message: 'Wrong page title'})
+}
+
+function createRandomFromRange() {
+    const min = expectedDCF.inputMin;
+    const max = expectedDCF.inputMax;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const createRandomInputInt = function () {
+    const min = createRandomFromRange()
+    const max = createRandomFromRange();
+    let randomULF;
+    let randomLLF;
+    if (max <= min) {
+        randomULF = min
+        randomLLF = max;
+    } else {
+        randomULF = max
+        randomLLF = min;
+    }
+    return {
+        ULF: randomULF,
+        LLF: randomLLF
+    }
+}
+
+function inputNumber(str, value) {
     if (str === "left") {
         if ($(selectorsCnt.lowerLimitField).isDisplayed()) {
             $(selectorsCnt.lowerLimitField).click();
@@ -18,4 +49,11 @@ function inputNumber (str, value) {
     }
 }
 
-module.exports = inputNumber;
+function clickOnBlackBtn(index) {
+    const button = $$(selectorsCnt.blackBtn)[index];
+    button.click();
+}
+
+
+export {inputNumber, open, createRandomInputInt,
+    clickOnBlackBtn};
--- test/defaultFunctionality.js
+++ test/defaultFunctionality.js
@@ -1,2 +1,29 @@
 import {selectorsGen, selectorsCnt} from './../data/selectors.json';
-import {expectedGen, expectedCnt} from './../data/expected.json';
+import {expectedGen, expectedCnt, expectedDCF} from './../data/expected.json';
+import {
+    open, inputNumber, createRandomInputInt,
+    clickOnBlackBtn
+} from "../helpers/functions";
+
+const randomULF = createRandomInputInt().ULF.toString();
+const randomLLF = createRandomInputInt().LLF.toString();
+
+describe('DEFAULT FUNCTIONALITY', () => {
+    before('open App', function () {
+        open();
+    })
+
+    it('TC-040 Subtract 1 gives -1', function () {
+        clickOnBlackBtn(0);
+        expect($(selectorsCnt.countValue)).toHaveText(expectedDCF.countValueTC040);
+    })
+    it('TC-041 Add 3 gives 2', function () {
+        clickOnBlackBtn(0);
+        expect($(selectorsCnt.countValue)).toHaveText(expectedDCF.countValueTC040);
+    })
+
+    it('TC-042 LLF accept 1', function () {
+        inputNumber('left', expectedDCF.inputMin.toString());
+        expect($(selectorsCnt.errorMsg)).not.toBeDisplayed();
+    })
+});
